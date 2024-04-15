050d1ecb14077bd097a35ae120e50ab791661b34
TATOAO <liangwentaoss@126.com>
2024-04-16 00:59:23 +0800

    import os and upate pack shell

--- nvim/packer_nvim.sh
+++ nvim/packer_nvim.sh
@@ -1,13 +1,23 @@
-home='/Users/tatoaoliang'
-config_desitination_folder="$home/Downloads/neovim_packed"
-dt=$(date '+%Y-%m-%d')
-# zip config 
-cd "$home/.config/"
-# zip -r "$config_desitination_folder/nvim_config_$dt.zip"  "./nvim/"
-tar -czvf "$config_desitination_folder/nvim_config_$dt.tar.gz" "./nvim"
-cd "$home/.local/share/"
-tar -czvf "$config_desitination_folder/nvim_package_$dt.tar.gz" "./nvim"
-# zip -r "$config_desitination_folder/nvim_package_$dt.zip"  "./nvim/"
+#!/bin/bash
 
+# Set the home directory based on the current user
+home="$HOME"
+
+# Define the destination folder for the config files
+config_destination_folder="$home/Downloads/neovim_packed"
+
+# Get the current date in YYYY-MM-DD format
+dt=$(date '+%Y-%m-%d')
+
+# Create the destination directory if it does not exist
+mkdir -p "$config_destination_folder"
+
+# Change to the .config directory and tar the nvim config
+cd "$home/.config/"
+tar -czvf "$config_destination_folder/nvim_config_$dt.tar.gz" "./nvim"
+
+# Change to the .local/share directory and tar the nvim data
+cd "$home/.local/share/"
+tar -czvf "$config_destination_folder/nvim_package_$dt.tar.gz" "./nvim"
 
 
