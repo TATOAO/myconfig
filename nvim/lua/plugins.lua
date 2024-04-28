e3e0ef33cc2a1fe94828ace96c22b2d7ddd5c818
TATOAO <liangwentaoss@126.com>
2024-04-28 19:34:06 +0800

    preveiw markdown may be fix?

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -43,8 +43,14 @@ require("lazy").setup({
 	'nvim-treesitter/nvim-treesitter-textobjects',
 
 	-- markdown
-	{"iamcco/markdown-preview.nvim",
-	    build = function() vim.fn["mkdp#util#install"]() end,
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	},
 	
 	-- Magma
