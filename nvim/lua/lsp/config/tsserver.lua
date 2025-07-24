6880ffbe153eaaaf9f1fa06f26c023d39e063e26
tatoao pro <tatoao@126.com>
2025-07-24 13:35:00 +0800

    uninstall ava

--- nvim/lua/lsp/config/tsserver.lua
+++ nvim/lua/lsp/config/tsserver.lua
@@ -2,14 +2,14 @@
 local lspconfig = require('lspconfig')
 
 -- Setup tsserver (TypeScript Server)
-lspconfig.ts_ls.setup {
-    on_attach = function(client, bufnr)
-        -- Optional: Disable tsserver formatting if you are using another formatter
-        client.server_capabilities.document_formatting = false
-    end,
-    flags = {
-        debounce_text_changes = 150,
-    },
-    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-    cmd = { "typescript-language-server", "--stdio" },
-}
+-- lspconfig.ts_ls.setup {
+--     on_attach = function(client, bufnr)
+--         -- Optional: Disable tsserver formatting if you are using another formatter
+--         client.server_capabilities.document_formatting = false
+--     end,
+--     flags = {
+--         debounce_text_changes = 150,
+--     },
+--     filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+--     cmd = { "typescript-language-server", "--stdio" },
+-- }
